540055d6a68fbef3ffb85a1bb5570ba01be86e07
stbaker <stf0204@gmail.com>
2016-07-16 17:10:06 -0400

    Created new functions to cache inverse of a matrix. 2016-07-16

--- README.md
+++ README.md
@@ -103,3 +103,7 @@ In order to complete this assignment, you must do the following:
 ### Grading
 
 This assignment will be graded via peer assessment.
+
+##
+## Changes were made to complete the makeCacheMatrix and cacheSolve functions. 
+##
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script contains 2 functions which will cache the inverse of a square 
+## invertible matrix rather than computing it repeatedly.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a special "matrix" object that can cache 
+## the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL 
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInv <- function(solve) i <<- solve
+        getInv <- function() i
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function computes the inverse of the "matrix" which was returned by 
+## the makeCacheMatrix function above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        if(class(try(solve(data),silent=T)) != "matrix") {
+                message("Matrix cannot be inverted")
+                return(i)
+        }
+        i <- solve(data,...)
+        x$setInv(i)
+        i
 }
